2f0685e5e319093bc3c53694b0c73a96ae67ca9c
Fernando-Fernandez <fmendes@hotmail.com>
2025-11-24 20:30:38 -0600

    Added LiquidAI's LFM model

--- public/models.js
+++ public/models.js
@@ -5,6 +5,7 @@
     'onnx-community/NanoChat-d32-ONNX': { friendly: 'NanoChat‑d32', dtype: 'q4', thinking: false },
     'onnx-community/gemma-3-270m-it-ONNX': { friendly: 'Gemma‑3‑270m‑IT', dtype: 'fp32', thinking: false },
     'onnx-community/DeepSeek-R1-Distill-Qwen-1.5B-ONNX': { friendly: 'DeepSeek R1 (Qwen‑1.5B)', dtype: 'q4f16', thinking: true },
+    'onnx-community/LFM2-1.2B-ONNX': { friendly: 'LFM2‑1.2B', dtype: 'q4', thinking: false }
   };
 
   try {
